072ab8ff07f27d19b94d23c4831135ce465493ce
Aswin Murali <aswinmurali.co@gmail.com>
2021-09-11 16:57:41 +0530

    removes main.py using __main__.py instead

--- decipher/main.py
+++
@@ -1,8 +0,0 @@
-import nltk
-
-text = 'hi'
-
-english_vocab = set(w.lower() for w in nltk.corpus.words.words())
-text_vocab = set(w.lower() for w in text if w.lower().isalpha())
-unusual = text_vocab.difference(english_vocab)
-
