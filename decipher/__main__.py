30b4a397cd15bf0fecb67e91cfaf0d46d967a7cd
Aswin Murali <aswinmurali.co@gmail.com>
2021-09-11 16:55:27 +0530

    implements base entry point for decipher by generating dataset

---
+++ decipher/__main__.py
@@ -0,0 +1,21 @@
+import nltk
+import itertools
+import multiprocessing as mp
+
+from decipher.utils.generator import DatasetGenerator
+
+manager = mp.Manager()
+generator = DatasetGenerator()
+generator.make()
+
+if __name__ == '__main__':
+    thread_id = manager.Value('i', 0)
+
+    with mp.Pool(generator.threads) as pool:
+        pool.starmap(
+            generator.generate_thread,
+            zip(
+                [nltk.corpus.words.words()] * generator.threads,
+                itertools.repeat(thread_id),
+            ),
+        )
