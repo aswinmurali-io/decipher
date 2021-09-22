25462c0c4035916aaab35e7f3f8ff2e9529e9a80
Aswin Murali <aswinmurali.co@gmail.com>
2021-09-22 08:30:05 +0530

    reimplements decipher parser API usage

--- decipher/__main__.py
+++ decipher/__main__.py
@@ -1,26 +1,6 @@
-import nltk
-import ctypes
-import itertools
-import multiprocessing as mp
-import decipher.utils.generator as gen
+from decipher.ui.cli import DecipherParserThread
 
-from decipher.ui.cli import DecipherParser
-from multiprocessing.managers import ValueProxy
-from typing import Final
-
-
-mgr: Final = mp.Manager()
-
-gen.make()
-keys: Final = [list(range(0, 100))] * gen.threads
-corpus_words: Final = gen.split(list(nltk.corpus.words.words()), splits=gen.threads)
 
 if __name__ == "__main__":
-    thread_id: ValueProxy = mgr.Value(ctypes.c_int, 0)
-
-    with mp.Pool(gen.threads) as pool:
-        pool.starmap(
-            gen.generate_thread, zip(corpus_words, itertools.repeat(thread_id), keys),
-        )
-
-    # DecipherParser().parse_args()
+    # This arg requires main
+    DecipherParserThread()
